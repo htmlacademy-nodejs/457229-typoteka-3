3ce4f1b9bd8e6de55e0f313f0408423fc98a1931
Anatoliy Avramenko <anatoliy.avramenko@xenial.com>
2020-10-12 21:52:11 +0300

    module2-task2 Перенос мок-текстов в файлы; Имплементация генерации плейсхолдеров

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -11,11 +11,23 @@ const {
   ANNOUNCE_LENGTH_LIMIT,
   MONTHS_AGO_LIMIT,
   DEFAULT_COUNT,
-  TITLES,
-  SENTENCES,
-  CATEGORIES,
   ExitCode,
 } = require(`../auxiliary/constants`);
+const FILE_SENTENCES_PATH = `${__dirname}/../../../data/sentences.txt`;
+const FILE_TITLES_PATH = `${__dirname}/../../../data/titles.txt`;
+const FILE_CATEGORIES_PATH = `${__dirname}/../../../data/categories.txt`;
+
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.split(`\n`)
+      .filter((el) => el.length > 0);
+
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
 
 const getRandomDate = () => {
   const getToday = () => new Date();
@@ -25,23 +37,27 @@ const getRandomDate = () => {
   return new Date(dateLowerLimit.getTime() + Math.random() * multiplier);
 };
 
-const getRandomDescription = (maxLength, minLength = 1) => {
+const getRandomDescription = (sentences, maxLength, minLength = 1) => {
   const targetLength = getRandomInt(minLength, maxLength);
 
-  return shuffle(SENTENCES).slice(0, targetLength).join(` `);
+  return shuffle(sentences).slice(0, targetLength).join(` `);
 };
 
-const generateAds = (count) => {
+const generateAds = (count, titles, categories, sentences) => {
   return Array(count).fill({}).map(() => ({
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     createdDate: getRandomDate(),
-    announce: getRandomDescription(ANNOUNCE_LENGTH_LIMIT),
-    fullText: getRandomDescription(SENTENCES.length, ANNOUNCE_LENGTH_LIMIT),
-    category: shuffle(CATEGORIES).slice(0, getRandomInt(1, CATEGORIES.length)),
+    announce: getRandomDescription(sentences, ANNOUNCE_LENGTH_LIMIT),
+    fullText: getRandomDescription(sentences, sentences.length, ANNOUNCE_LENGTH_LIMIT),
+    category: shuffle(categories).slice(0, getRandomInt(1, categories.length)),
   }));
 };
 
 const runGenerateAndSaveScenario = async (args) => {
+  const sentences = await readContent(FILE_SENTENCES_PATH);
+  const titles = await readContent(FILE_TITLES_PATH);
+  const categories = await readContent(FILE_CATEGORIES_PATH);
+
   const [count] = args;
   const adsCount = Number.parseInt(count, 10) || DEFAULT_COUNT;
   if (adsCount > MAX_ADS_COUNT) {
@@ -49,7 +65,7 @@ const runGenerateAndSaveScenario = async (args) => {
     process.exit(ExitCode.SUCCESS);
   }
 
-  const content = JSON.stringify(generateAds(adsCount));
+  const content = JSON.stringify(generateAds(adsCount, titles, categories, sentences));
   try {
     await fs.writeFile(`${__dirname}/../../../mocks.json`, content);
     console.info(chalk.green(`Operation success. File created.`));
