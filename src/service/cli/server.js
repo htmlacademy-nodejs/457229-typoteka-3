ad5b1b2fce59502830231a86e92a11a945c14aac
Anatoliy Avramenko <anatoliy.avramenko@xenial.com>
2020-10-12 22:24:03 +0300

    module2-task3 Добавление сервера; Подключение nodemon

---
+++ src/service/cli/server.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const chalk = require(`chalk`);
+const http = require(`http`);
+const fs = require(`fs`).promises;
+const DEFAULT_PORT = `3000`;
+const FILENAME = `mocks.json`;
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
+
+const sendResponse = (res, statusCode, message) => {
+  const template = `
+    <!Doctype html>
+      <html lang="ru">
+      <head>
+        <title>With love from Node</title>
+      </head>
+      <body>${message}</body>
+    </html>`.trim();
+
+  res.statusCode = statusCode;
+  res.writeHead(statusCode, {
+    'Content-Type': `text/html; charset=UTF-8`,
+  });
+
+  res.end(template);
+};
+
+const onClientConnect = async (req, res) => {
+  const notFoundMessageText = `Not found`;
+
+  switch (req.url) {
+    case `/`:
+      try {
+        const fileContent = await fs.readFile(FILENAME);
+        const mocks = JSON.parse(fileContent);
+        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
+        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
+      } catch (err) {
+        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+      }
+
+      break;
+    default:
+      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+      break;
+  }
+};
+
+const createServer = (port) => {
+  http.createServer(onClientConnect)
+    .listen(port)
+    .on(`listening`, (err) => {
+      if (err) {
+        return console.error(`Ошибка при создании сервера`, err);
+      }
+
+      return console.info(chalk.green(`Ожидаю соединений на ${port}`));
+    });
+};
+
+module.exports = {
+  name: `--server`,
+  run(args) {
+    const [customPort] = args;
+    const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
+    createServer(port);
+  }
+};
